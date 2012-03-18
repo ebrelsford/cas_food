f256b68efa36a167bcbaf7db11f9b247dfdf1a7c
Eric Brelsford <ebrelsford@gmail.com>
2012-03-18 12:50:12 -0400

    more useful school admin

--- schools/admin.py
+++ schools/admin.py
@@ -5,5 +5,6 @@ from models import School
 class SchoolAdmin(admin.ModelAdmin):
     search_fields = ('name', 'address',)
     list_display = ('name', 'address', 'city', 'type', 'grades', 'has_content',)
+    list_filter = ('city', 'type', 'participates_in_wellness_in_the_schools',)
 
 admin.site.register(School, SchoolAdmin)
