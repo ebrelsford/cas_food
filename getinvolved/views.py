dbbce2b9760842af9eeeb0be21890456e6a12465
Eric Brelsford <ebrelsford@gmail.com>
2012-04-27 13:00:19 -0400

    getinvolved permissions

--- getinvolved/views.py
+++ getinvolved/views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse
 from django.views.generic import CreateView, UpdateView
 
 from forms import PostCreateForm, PostUpdateForm
+from generic.views import LoginRequiredMixin, PermissionRequiredMixin
 from models import Post
 
 class PostFormMixin(object):
@@ -11,16 +12,20 @@ class PostFormMixin(object):
     def get_success_url(self):
         return reverse('getinvolved_post_list')
 
-class PostCreateView(PostFormMixin, CreateView):
+class PostCreateView(LoginRequiredMixin, PermissionRequiredMixin,
+                     PostFormMixin, CreateView):
     form_class = PostCreateForm
+    permission = 'getinvolved.create_post'
 
     def get_initial(self):
         initial = super(PostCreateView, self).get_initial()
         initial['added_by'] = self.request.user
         return initial
 
-class PostUpdateView(PostFormMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, PermissionRequiredMixin, 
+                     PostFormMixin, UpdateView):
     form_class = PostUpdateForm
+    permission = 'getinvolved.change_post'
 
     def get_initial(self):
         initial = super(PostUpdateView, self).get_initial()
