9635872c2237d4d4ea3444b93ffb03b896289670
Eric Brelsford <ebrelsford@gmail.com>
2012-08-15 22:50:57 -0400

    food admin

--- food/admin.py
+++ food/admin.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin.widgets import FilteredSelectMultiple
 
 from models import Callout, Dish, DishIngredient, Ingredient, Meal, Nutrient, NutritionFact
 
@@ -10,6 +12,7 @@ class DishAdmin(admin.ModelAdmin):
     list_display = ('name', 'school_type',)
     list_filter = ('school_type',)
     search_fields = ('name',)
+    ordering = ('name',)
 
 class DishIngredientAdmin(admin.ModelAdmin):
     search_fields = ('dish', 'ingredient',)
@@ -20,15 +23,28 @@ class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 class NutrientAdmin(admin.ModelAdmin):
-    search_fields = ('name',)
     list_display = ('name',)
+    ordering = ('name',)
+    search_fields = ('name',)
 
 class NutritionFactAdmin(admin.ModelAdmin):
-    search_fields = ('dish__name', 'nutrient__name',)
     list_display = ('dish', 'nutrient',)
+    list_filter = ('dish__school_type',)
+    ordering = ('dish__name', 'nutrient',)
+    search_fields = ('dish__name', 'nutrient__name',)
+
+class MealAdminForm(forms.ModelForm):
+    dishes = forms.ModelMultipleChoiceField(
+        queryset=Dish.objects.all().order_by('name'),
+        widget=FilteredSelectMultiple('dishes', is_stacked=False),
+    )
 
 class MealAdmin(admin.ModelAdmin):
-    list_display = ('date', 'school_type')
+    date_hierarchy = 'date'
+    form = MealAdminForm
+    list_display = ('date', 'school_type',)
+    list_filter = ('date', 'school_type',)
+    ordering = ('date', 'school_type',)
 
 admin.site.register(Callout, CalloutAdmin)
 admin.site.register(Dish, DishAdmin)
