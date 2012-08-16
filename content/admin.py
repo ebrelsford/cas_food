b8bb2d40ddbd157b003f601f632d9e7af614cc3c
Eric Brelsford <ebrelsford@gmail.com>
2012-08-15 22:29:09 -0400

    content admins

--- content/admin.py
+++ content/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 from ckeditor.widgets import CKEditorWidget
 
-from content.models import Section
+from content.models import Note, Picture, Section, Video
 
 class SectionAdminForm(forms.ModelForm):
     """Override standard admin form for flatpages"""
@@ -12,8 +12,20 @@ class SectionAdminForm(forms.ModelForm):
     class Meta:
         model = Section
 
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('added_by', 'added',)
+
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'added_by', 'added',)
+
 class SectionAdmin(admin.ModelAdmin):
     form = SectionAdminForm
     list_display = ('title', 'content_type',)
 
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'added_by', 'added',)
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Picture, PictureAdmin)
 admin.site.register(Section, SectionAdmin)
+admin.site.register(Video, VideoAdmin)
