01dc8018b2e794dc3ba694645a42c57dd05ad100
Eric Brelsford <ebrelsford@gmail.com>
2012-09-09 18:07:51 -0400

    sidebar

--- food/context_processors.py
+++ food/context_processors.py
@@ -6,8 +6,13 @@ def todays_menu(request):
     """
     Add today's elementary meals to the context, for use in the sidebar.
     """
-    # TODO cache?
     return {
-        'todays_menu': Meal.objects.filter(school_type='elementary',
-                                           date=date.today()),
+        'todays_menu': Meal.objects.filter(
+            school_type='elementary',
+            date=date.today()
+        ),
+        'todays_menu_wits': Meal.objects.filter(
+            school_type='wits',
+            date=date.today()
+        ),
     }
