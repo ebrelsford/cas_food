6bbdb6a82f98ed017fab49a7d02c2b5aac1b2b18
Eric Brelsford <ebrelsford@gmail.com>
2012-09-19 14:53:11 -0400

    don't specify borough/school type in mobile map

--- school_food_site/media/js/mobile/init.js
+++ school_food_site/media/js/mobile/init.js
@@ -1,8 +1,6 @@
 $(document).on('pageshow', '#map-page', function(event) {
     $('#map-page #map').height($(window).height() - $('#map-page [data-role="header"]').height())
     $('#map').schoolmap({
-        boroughs: 'Brooklyn,Manhattan',   
-        types: 'Elementary',
         mobile: true,
     });
     $('form.school-search').schoolsearch({
