bb2ed4ff5c2ac38f94bbca67e9151b80e0ba3966
Eric Brelsford <ebrelsford@gmail.com>
2012-09-19 15:34:47 -0400

    consolidate_dish with MealDish

--- food/util.py
+++ food/util.py
@@ -1,4 +1,5 @@
 from alias.models import Alias
+from food.models import MealDish
 
 def add_alias(dish, alias_text):
     alias = Alias(
@@ -13,11 +14,23 @@ def consolidate_dish(dish, duplicates):
     Remove duplicate dishes, add aliases to the dish we're consolidating on.
     """
     for duplicate_dish in duplicates:
-        add_alias(dish, duplicate_dish.name)
+        alias = add_alias(dish, duplicate_dish.name)
         for meal in duplicate_dish.meal_set.all():
-            meal.dishes.remove(duplicate_dish)
-            meal.dishes.add(dish)
-            meal.save()
+            # delete duplicate dish
+            MealDish.objects.filter(
+                dish=duplicate_dish,
+                meal=meal,
+            ).delete()
+
+            # add consolidated dish, with alias
+            mealdish = MealDish(
+                alias=alias,
+                dish=dish,
+                meal=meal,
+            )
+            mealdish.save()
 
         # NB: assumes we can safely delete DishIngredients on duplicate
         duplicate_dish.delete()
+
+# TODO consolidate dish without creating an Alias
